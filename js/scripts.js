57a02f0cb4c92b5dab1dd18ffa520daace077fa0
Wiktor <wiktorszczepaniak1@gmail.com>
2017-08-07 21:25:48 +0200

    Exercise 9_5

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,7 @@
-/*function drawTree(n) {
-
-for ( var i=0; i<n; i++) {
-	var star =("");
-
-	for (var j=0; j<=i; j++){
-		console.log("*");
-	}
-
-}
-}
-
-drawTree(5) */
-
-	
-function drawTree(n) {
-	for ( var i=1; i<=n; i++) {  ///  i =rows
-		var star=(" ");
-	    {
-		for ( var j=1; j<=i; j++) //// j =columns
-			star += '*';
-		}
-		console.log(star)
-	}
-}
-
-drawTree(10); // 
 
 
-function drawTree(n) {
-	for ( var i=1; i<=n; i++) {
-		var star=(" ");
-		{
-		for (var star=n-i; star>=1; star--) {
-			console.log(star)
-		}
-		}
-	}  ///  i =rows
+var x = document.getElementsByClassName('button');
+for (i=0; i<=x.length; i++) {
+var text =x[i].innerText;
+alert(text);
 }
