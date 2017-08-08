982349c9be4e3caa74c833555e2c3a03752edd7d
Wiktor <wiktorszczepaniak1@gmail.com>
2017-08-08 17:29:13 +0200

    Corrections

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,8 @@ for ( var i=0; i<n; i++) {
 drawTree(5) */
 
 	
-function drawTree(n) {
-	for ( var i=1; i<=n; i++) {  ///  i =rows
+function drawTree(n){
+	for ( var i=1; i<=n; i++){  ///  i =rows
 		var star=(" ");
 	    {
 		for ( var j=1; j<=i; j++) //// j =columns
@@ -24,16 +24,15 @@ function drawTree(n) {
 	}
 }
 
-drawTree(10); // 
+drawTree(10); 
 
-
-function drawTree(n) {
-	for ( var i=1; i<=n; i++) {
-		var star=(" ");
-		{
-		for (var star=n-i; star>=1; star--) {
-			console.log(star)
-		}
-		}
-	}  ///  i =rows
-}
+//function drawTreeSpaces(n) {
+//	for ( var i=1; i<=n; i++) {
+//		var star=(" ");
+//		{
+//		for (var star=n-i; star>=1; star--) {
+//			console.log(star)
+//		}
+//		}
+//	} 
+//}
