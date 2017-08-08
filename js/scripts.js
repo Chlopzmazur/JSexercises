eee71bfe16153ca3e3e45cdeb8532d4653a506f7
Wiktor <wiktorszczepaniak1@gmail.com>
2017-08-08 17:22:12 +0200

    Corrected the exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
-var text = ('Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.')
-var dinosaur = ('triceratops')
-var dinosaurUpperCased = dinosaur.toUpperCase()
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
+var dinosaurUpperCased = dinosaur.toUpperCase();
 
 var textReplaced = text.replace('Velociraptor', dinosaurUpperCased);
 
@@ -9,5 +9,5 @@ console.log(textReplaced);
 
 console.log(textReplaced.length);
 
-var partOftextReplaced = textReplaced.slice (0,72);
+var partOftextReplaced = textReplaced.slice(0,72);
 console.log (partOftextReplaced);
