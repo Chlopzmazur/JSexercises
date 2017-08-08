36df3048722ed19c960b54e59b87099ec138137d
Wiktor <wiktorszczepaniak1@gmail.com>
2017-08-08 16:33:57 +0200

    Added corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,21 @@
-var a = prompt('Enter the width of triangle base');
-var	h = prompt('Enter the height of a triangle');
 
-function getTriangleArea (a, h) {
+function getTriangleArea (a, h){
+var a = Number(prompt ('Enter the width of triangle base')),
+	h = Number(prompt ('Enter the height of a triangle')),
+	TriangleArea = (a*h/2);
 
-  var a = Number(prompt ('Enter the width of triangle base'));
-  var h = Number(prompt ('Enter the height of a triangle'));
-  var TriangleArea = (a*h/2);
-
-  if ((a <= 0) || (h <= 0)) {
-
+	if ((a <= 0) || (h <= 0)){
     console.log("Invalid data");
-
 }
-     else {
+    else {
     alert (TriangleArea);
-    return TriangleArea;  
-
+    return TriangleArea;
 }
-
 }
-
-
-var triangle1Area = getTriangleArea(10, 15);
-var	triangle2Area = getTriangleArea(8, 6);
-var	triangle3Area = getTriangleArea(4, 12);
-
+var triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(8, 6),
+	triangle3Area = getTriangleArea(4, 12);
+	
 console.log (getTriangleArea(a, h));
 console.log (triangle1Area);
 console.log (triangle2Area);
