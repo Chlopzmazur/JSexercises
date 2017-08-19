3e904ad4606a9af970a8e80b06ff0ebe59f676d6
Wiktor <wiktorszczepaniak1@gmail.com>
2017-08-19 20:47:45 +0200

    Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,17 @@
-
-function getTriangleArea (a, h){
-var a = Number(prompt ('Enter the width of triangle base')),
-	h = Number(prompt ('Enter the height of a triangle')),
-	TriangleArea = (a*h/2);
-
+function getTriangleArea(a, h){
+var	triangleArea = (a*h/2);
 	if ((a <= 0) || (h <= 0)){
-    console.log("Invalid data");
+    alert("Invalid data");
 }
     else {
-    alert (TriangleArea);
-    return TriangleArea;
+    console.log(triangleArea);  
 }
-}
-var triangle1Area = getTriangleArea(10, 15),
-	triangle2Area = getTriangleArea(8, 6),
-	triangle3Area = getTriangleArea(4, 12);
-	
-console.log (getTriangleArea(a, h));
-console.log (triangle1Area);
-console.log (triangle2Area);
-console.log (triangle3Area);
+	return;
+};
 
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(8, 5);
+var triangle3Area = getTriangleArea(7, 4);
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
