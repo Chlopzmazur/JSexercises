ac92a5f433f3769d8815a577f0c86715f19b9cbb
Wiktor <wiktorszczepaniak1@gmail.com>
2017-08-19 21:43:43 +0200

    new commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
-var add = document.getElementById('addElem'),
-	list = document.getElementById('list');
+function Phone(brand, price, color, critics)	{
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+	this.critics = critics;
+}
 
-add.addEventListener('click',function(e){
-	var element = document.createElement('li');
-	element.innerHTML = 'item ';
-	element.innerHTML += document.getElementsByTagName('li').length;
-	list.appendChild(element);
-})
+Phone.prototype.printInfo = function() {
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". According to critis this phone has rating: " + this.critics + "." );
+};
 
+var iPhone6S = new Phone("Apple", 2250, "silver", "9/10");
+var SamsungGalaxyS6 = new Phone("Samsung", 1200, "black", "9/10");
+var OnePlusOne = new Phone("One Plus", 1600, "black", "8,5/10");
 
+iPhone6S.printInfo();
+SamsungGalaxyS6.printInfo();
+OnePlusOne.printInfo();
