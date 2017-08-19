9d95d999ca4474456f18b9d7a02894f932cbb170
Wiktor <wiktorszczepaniak1@gmail.com>
2017-08-19 21:26:21 +0200

    c

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,14 @@
-/*function drawTree(n) {
-
-for ( var i=0; i<n; i++) {
-	var star =("");
-
-	for (var j=0; j<=i; j++){
-		console.log("*");
-	}
-
-}
-}
-
-drawTree(5) */
 
 	
 function drawTree(n){
-	for ( var i=1; i<=n; i++){  ///  i =rows
+	for (var i=1; i<=n; i++){  ///  i =rows
 		var star=(" ");
 	    {
-		for ( var j=1; j<=i; j++) //// j =columns
+		for (var j=1; j<=i; j++) //// j =columns
 			star += '*';
 		}
 		console.log(star)
 	}
 }
+drawTree(10);
 
-drawTree(10); 
-
-//function drawTreeSpaces(n) {
-//	for ( var i=1; i<=n; i++) {
-//		var star=(" ");
-//		{
-//		for (var star=n-i; star>=1; star--) {
-//			console.log(star)
-//		}
-//		}
-//	} 
-//}
